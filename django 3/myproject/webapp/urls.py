03ef0ad6abfadb115547ae96e630a1b2ecf6f9fb
Arunkarthick1234567 <157337096+Arunkarthick1234567@users.noreply.github.com>
2024-10-04 20:15:38 +0530

    Add files via upload

---
+++ django 3/myproject/webapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.odd_even_checker, name='odd_even_checker'),
+    path('filter/', views.name_filter, name='name_filter'),
+]
+
